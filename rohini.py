068b564860b8722ad2d225636226530b90a2d4e8
rohini aher <rohiniaher858@gmail.com>
2026-02-12 12:46:51 +0530

    first commit

--- rohini.py
+++ rohini.py
@@ -1,20 +1,40 @@
 import random
-while True:
 
-    a = random.randint(0,9)
-    b = random.randint(0,9)
+def generate_question():
+    
+    a = random.randint(1, 9)
+    b= random.randint(1, 9)
+    return a, b
 
- 
-    sum = a + b
-    print(" ",a)
-    print("+",b)
-    print("----")
+def ask_question():
+    a, b = generate_question()
+    correct_answer = a + b
 
-    ans = int(input("Answer:"))
-
-    if ans == sum:
-        print("very good")
+    while True:
         
-    else:
-        print("Hey! Try again")
+        print("  {a}")
+        print("+ {b}")
+        print("----")
+
+        try:
+            student_answer = int(input("Your answer: "))
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+
+        if student_answer == correct_answer:
+            print("Very good! ")
+            break
+        else:
+            print("Hey! Try again ")
         
+def main():
+    print("Welcome to Addition Practice!")
+    print("Solve the problems. Press Ctrl+C to exit.\n")
+
+    while True:
+        ask_question()
+
+if __name__ == "__main__":
+
+    main()
