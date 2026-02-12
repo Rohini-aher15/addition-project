a74a3437e4b21a29fcd1a6062e805920afc4f7ca
rohini aher <rohiniaher858@gmail.com>
2026-02-12 11:30:08 +0530

    first commit

--- rohini.py
+++ rohini.py
@@ -1,16 +1,20 @@
 import random
-a = random.randint(1,9)
-b = random.randint(1,9)
-
-sum = a + b
- 
 while True:
-    print(a)
+
+    a = random.randint(0,9)
+    b = random.randint(0,9)
+
+ 
+    sum = a + b
+    print(" ",a)
     print("+",b)
+    print("----")
+
     ans = int(input("Answer:"))
 
     if ans == sum:
         print("very good")
-        break
+        
     else:
         print("Hey! Try again")
+        
